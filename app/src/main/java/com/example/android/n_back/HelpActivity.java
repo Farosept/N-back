91da5ca824af8d60af454ddcbcefaab1ba349cf4
Артем Хорев <farosept@gmail.com>
2017-10-22 00:34:58 +0700

    add fragments

--- app/src/main/java/com/example/android/n_back/HelpActivity.java
+++ app/src/main/java/com/example/android/n_back/HelpActivity.java
@@ -1,33 +1,29 @@
 package com.example.android.n_back;
 
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class HelpActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-    private HelpFragment helpFragment;
-    private HomeFragment homeFragment;
+    private Intent intentMain;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            FragmentTransaction transaction = getFragmentManager().beginTransaction();
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    transaction.replace(R.id.container,homeFragment);
-                    transaction.commit();
+                    startActivity(intentMain);
+
                     return true;
                 case R.id.navigation_help:
-                    transaction.replace(R.id.container,helpFragment);
-                    transaction.commit();
+                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_settings:
                     mTextMessage.setText(R.string.title_notifications);
@@ -36,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
-
             return false;
         }
 
@@ -45,17 +40,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_help);
+
+        intentMain = new Intent(this, HelpActivity.class);
+
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        helpFragment = new HelpFragment();
-        homeFragment = new HomeFragment();
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.id.container,homeFragment);
-        transaction.commit();
     }
 
 }
